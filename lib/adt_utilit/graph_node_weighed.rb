6e6b993fca1b2799b008d1786389aa2d3bf3605c
Hirosvk <kjnhiroyuki@hotmail.com>
2016-09-04 21:26:56 -0700

    Dijkstra's algorithm functional

--- lib/adt_utilit/graph_node_weighed.rb
+++ lib/adt_utilit/graph_node_weighed.rb
@@ -1,4 +1,103 @@
 require_relative "graph_node"
+require 'set'
 
 class GraphNodeWeighed < GraphNode
+  def connect(node, weight)
+    raise "invalid weight: needs to be integer" unless weight.is_a?(Integer)
+    @children << [node, weight]
+  end
+
+  def weight(node)
+    target = node.is_a?(self.class) ? node.value : node
+    found = @children.find{ |el| el[0].value == target }
+    return nil if found.nil?
+    found[1]
+  end
+
+  # def shortest_paths_to_all_nodes
+  #   @path_weight = Hash.new(Array.new)
+  #   @path_weight[self.value] = [0, self.value]
+  #   @checked_node_values = Set.new
+  #
+  #   self._shortest_paths_to_all_nodes(self)
+  #
+  #   path_weight
+  # end
+
+  # def _shortest_paths_to_all_nodes(initial_node)
+  #   return if self.children.length < 1 || initial_node.checked_node_values.include?(self.value)
+  #
+  #   initial_node.checked_node_values.add(self.value)
+  #   children_queue = self.children.map(&:first).sort{|el1, el2| self.weight(el1) <=> self.weight(el2)}
+  #
+  #   children_queue.each do |child|
+  #     current_path_weight = initial_node.path_weight[child.value][0]
+  #     path_weight_to_self = initial_node.path_weight[self.value][0]
+  #     new_path_weight = path_weight_to_self + self.weight(child.value)
+  #     debugger if current_path_weight && current_path_weight > new_path_weight
+  #     if current_path_weight.nil? || (current_path_weight > new_path_weight)
+  #       initial_node.path_weight[child.value] = [new_path_weight, self.value]
+  #     end
+  #     child._shortest_paths_to_all_nodes(initial_node)
+  #   end
+  # end
+  # attr_accessor :path_weight, :checked_node_values
+
+
+  def shortest_paths_to_all_nodes
+    path_weight = Hash.new(Array.new)
+    path_weight[self.value] = [0, self.value]
+    checked_node_values = Set.new
+
+    checked_node_values.add(self.value)
+    children_queue = [[self, self.sort_children]]
+    # you need to start with the child whose weight is shortest to 'self'
+    # otherwise, it might miss the shortest path when, for example, a certain
+    # node has been checked through a longer path, and that node won't be checked
+    # again.
+
+    until children_queue.empty?
+      current_queue = children_queue.shift
+      current_node = current_queue[0]
+      checked_node_values.add(current_node.value)
+      current_queue = current_queue[1]
+
+      until current_queue.empty?
+        child = current_queue.shift
+        current_path_weight = path_weight[child.value][0]
+        path_weight_to_self = path_weight[current_node.value][0]
+        new_path_weight = path_weight_to_self + current_node.weight(child.value)
+
+        if current_path_weight.nil? || (current_path_weight > new_path_weight)
+          path_weight[child.value] = [new_path_weight, current_node.value]
+        end
+        new_children = child.sort_children.reject{|el| checked_node_values.include?(el.value)}
+        children_queue << [child, new_children] unless new_children.empty?
+      end
+    end
+    path_weight
+  end
+
+  def sort_children
+    self.children.map(&:first).sort{|el1, el2| self.weight(el1) <=> self.weight(el2)}
+  end
+
+  def shortest_path_to(node)
+    target = node.is_a?(self.class) ? node.value : node
+    path_weight = shortest_paths_to_all_nodes
+    result = []
+
+    unless path_weight[target][1].nil?
+      prev_value = target
+      while true
+        result << prev_value
+        prev_value = path_weight[prev_value][1]
+        break if prev_value == self.value
+      end
+      result << self.value
+    end
+    result.reverse
+  end
+
+
 end
--- spec/graph_node_weighed_spec.rb
+++ spec/graph_node_weighed_spec.rb
@@ -2,20 +2,82 @@ require "rspec"
 require "adt_utilit/graph_node_weighed"
 
 describe GraphNodeWeighed do
+  subject(:node_a){ GraphNodeWeighed.new("a") }
+  let(:node_b)   { GraphNodeWeighed.new("b") }
+  let(:node_c)    { GraphNodeWeighed.new("c") }
+  let(:node_d)  { GraphNodeWeighed.new("d") }
+  let(:node_e)   { GraphNodeWeighed.new("e") }
+  let(:node_f)   { GraphNodeWeighed.new("f") }
+  let(:node_g)   { GraphNodeWeighed.new("g") }
+  let(:node_h)   { GraphNodeWeighed.new("h") }
+  let(:node_i)   { GraphNodeWeighed.new("i") }
+
+  before(:each) {
+    node_a.connect(node_b, 5)
+    node_a.connect(node_c, 3)
+    node_a.connect(node_e, 2)
+
+    node_b.connect(node_d, 2)
+
+    node_c.connect(node_b, 1)
+    node_c.connect(node_d, 1)
+
+    node_d.connect(node_a, 1)
+    node_d.connect(node_g, 2)
+    node_d.connect(node_h, 1)
+
+    node_e.connect(node_a, 1)
+    node_e.connect(node_h, 4)
+    node_e.connect(node_i, 7)
+
+    node_f.connect(node_b, 3)
+    node_f.connect(node_g, 1)
+
+    node_g.connect(node_c, 3)
+    node_g.connect(node_i, 2)
+
+    node_h.connect(node_c, 2)
+    node_h.connect(node_g, 2)
+    node_h.connect(node_f, 2)
+  }
   describe "#children" do
     it "returns an array whose first element is the child, and the second element is the weight" do
-
+      expect(node_a.children).to eq([[node_b, 5], [node_c, 3], [node_e, 2]])
     end
   end
 
   describe "#weight" do
-    it "returns the weight of the edge from the receiver node to the argument node"
+    it "returns the weight of the edge from the receiver node to the argument node" do
+      expect(node_b.weight(node_d)).to eq(2)
+    end
+    it "can take the value instead of the node" do
+      expect(node_e.weight("i")).to eq(7)
+    end
+
+    it "returns nil if there is no edge to the node or value" do
+      expect(node_a.weight(node_i)).to eq(nil)
+      expect(node_e.weight("f")).to eq(nil)
+    end
   end
 
   describe "#connect" do
-    it "raises error unless the node is non-weighed"
-    it "connects the node with the weight of the edge"
+    it "raises error unless the weight is integer"
   end
 
+  describe "#shortest_path_to" do
+    it "returns the shortest path" do
+      expect(node_a.shortest_path_to(node_i)).to eq(["a", "c", "d", "g", "i"])
+      debugger
+    end
+  end
+
+  describe "#shortest_paths_to_all_nodes" do
+    it "returns a map of the shortest distance to each node" do
+      result = node_a.shortest_paths_to_all_nodes
+      expect(result["b"]).to eq([4, "c"])
+      expect(result["i"]).to eq([8, "g"])
+      expect(result["h"]).to eq([5, "d"])
+    end
+  end
 
 end
